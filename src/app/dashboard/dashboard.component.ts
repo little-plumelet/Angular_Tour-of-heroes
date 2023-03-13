4d07c82ebe8e44d3f35480194580f4cc0bc73f98
Anastasiia Perova <perovaa.24@gmail.com>
2023-03-13 19:27:22 +0400

    add rout '/dashboard' & component dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IHero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  heroes?: IHero[]
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => this.heroes = heroes.slice(0, 5))
+  }
+ }
